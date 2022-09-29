f02c54228b60ed347e3851ae407d5d733ef18f46
FranSemprini <fransemprini04@gmail.com>
2022-09-29 14:59:30 -0400

    mobile menu bug fixed

--- script.js
+++ script.js
@@ -1,15 +1,31 @@
-
 let botones = document.querySelectorAll('.btn1')
 let toggler = document.querySelector('.navbar-toggler')
+let mediaQuery = window.matchMedia('(max-width: 1023px)')
 
 const fBotones = () => {
-    botones.forEach(element => {
-        element.addEventListener('click', () => {
-            setTimeout(() => {
-                toggler.click()
-            }, 500);
-        })
-    });
+    handleQuery(mediaQuery)
 }
+
+const timeOut = () => {
+    setTimeout(() => {
+        toggler.click()
+    }, 500);
+}
+
+const handleQuery = (e) => {
+    console.log(e)
+    if (e.matches) {
+        botones.forEach(e => {
+            e.addEventListener('click', timeOut)
+        });
+    }
+    else {
+        botones.forEach(e => {
+            e.removeEventListener('click', timeOut)
+        })
+    }
+}
+
+mediaQuery.addEventListener('change', handleQuery)
 
 fBotones()
